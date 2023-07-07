5777ff2bad6e18cb3d532d83299ebb29b5e90f26
MushfikYeasirPk <pkm2243@mail.com>
2023-07-07 22:12:59 +0600

    set tab item

--- src/Components/Tab Data/TabData.jsx
+++ src/Components/Tab Data/TabData.jsx
@@ -6,15 +6,31 @@ function TabData() {
    <>
     <Tabs>
     <TabList>
-      <Tab>Title 1</Tab>
-      <Tab>Title 2</Tab>
+      <Tab>All Menu</Tab>
+      <Tab>Breakfast</Tab>
+      <Tab>Lunch</Tab>
+      <Tab>Dinner</Tab>
+      <Tab>Budget Meal</Tab>
+      <Tab>Buffet</Tab>
     </TabList>
 
     <TabPanel>
-      <h2>Any content 1</h2>
+      <h2>Menu item</h2>
     </TabPanel>
     <TabPanel>
-      <h2>Any content 2</h2>
+      <h2>Breakfast item</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Lunch item</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Dinner Meal</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Budget Item</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Buffet</h2>
     </TabPanel>
   </Tabs>
   </>
