3ce0c7e8148a2dddcb6f6d8b3671354f4b0604d5
MushfikYeasirPk <pkm2243@mail.com>
2023-07-08 09:43:51 +0600

    update react tab

--- src/Components/Tab Data/TabData.jsx
+++ src/Components/Tab Data/TabData.jsx
@@ -3,37 +3,31 @@ import 'react-tabs/style/react-tabs.css';
 
 function TabData() {
   return (
-   <>
-    <Tabs>
-    <TabList className='flex items-center justify-center gap-10'>
-      <Tab>All Menu</Tab>
-      <Tab>Breakfast</Tab>
-      <Tab>Lunch</Tab>
-      <Tab>Dinner</Tab>
-      <Tab>Budget Meal</Tab>
-      <Tab>Buffet</Tab>
-    </TabList>
+    <>
+      <Tabs>
+        <TabList className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 container mx-auto lg:h-48 sm:h-72 items-center text-center justify-center gap-5 pb-10">
+   
+          <Tab className='p-2 sm:p-1 border-2 border-orange-700 text-orange-700 rounded'>All Menu</Tab>
+          <Tab className='p-2 border-2 border-orange-700 text-orange-700 rounded'>Breakfast</Tab>
+          <Tab className='p-2 border-2 border-orange-700 text-orange-700 rounded'>Lunch</Tab>
+          <Tab className='p-2 border-2 border-orange-700 text-orange-700 rounded'>Dinner</Tab>
 
-    <TabPanel>
-      <h2>Menu item</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Breakfast item</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Lunch item</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Dinner Meal</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Budget Item</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Buffet</h2>
-    </TabPanel>
-  </Tabs>
-  </>
+        </TabList>
+
+        <TabPanel>
+          <h2>Menu item</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Breakfast item</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Lunch item</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Dinner Meal</h2>
+        </TabPanel>
+      </Tabs>
+    </>
   )
 }
 
